55cba223cc9692eb2e85188bc24d4a29764ed2ff
JoshiDevelopment <joshuamcrawford999@gmail.com>
2023-05-23 20:43:08 -0700

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { ReactComponent as Background } from '../src/login.svg';
 
 
 
 const App = () => {
   return (
-    <div className="w-screen h-screen">
-    <Background className="absolute w-full h-full" />
-    {/* Rest of your component's content */}
-  </div>
+    <div>App</div>
   )
 }
 
